c7706eb622524a8e7395df6a5f367fbaa57eff42
olebeck <31539311+olebeck@users.noreply.github.com>
2022-08-27 23:51:49 +0200

    add seagrass functionality

--- server/block/seagrass.go
+++ server/block/seagrass.go
@@ -4,6 +4,7 @@ import (
 	"github.com/df-mc/dragonfly/server/block/cube"
 	"github.com/df-mc/dragonfly/server/item"
 	"github.com/df-mc/dragonfly/server/world"
+	"github.com/go-gl/mathgl/mgl64"
 )
 
 // Seagrass is a non-solid plant block that generates in all oceans, except frozen oceans.
@@ -14,6 +15,99 @@ type Seagrass struct {
 
 	// Type is the type of seagrass
 	Type SeagrassType
+}
+
+// canSurvive ...
+func (s Seagrass) canSurvive(pos cube.Pos, w *world.World, ignorePartner bool) bool {
+	below := w.Block(pos.Side(cube.FaceDown))
+	above := w.Block(pos.Side(cube.FaceUp))
+
+	if !ignorePartner {
+		if s.Type == TopSeagrass() {
+			if bottom, ok := below.(Seagrass); ok {
+				if bottom.Type != BottomSeagrass() {
+					return false
+				}
+			} else {
+				return false
+			}
+		}
+
+		if s.Type == BottomSeagrass() {
+			if top, ok := above.(Seagrass); ok {
+				if top.Type != TopSeagrass() {
+					return false
+				}
+			} else {
+				return false
+			}
+		}
+	}
+
+	if liquid, ok := w.Liquid(pos); ok {
+		_, is_water := liquid.(Water)
+		if !is_water {
+			return false
+		}
+	} else {
+		return false
+	}
+
+	if s.Type != TopSeagrass() {
+		if !below.Model().FaceSolid(pos.Side(cube.FaceDown), cube.FaceUp, w) {
+			return false
+		}
+	}
+	return true
+}
+
+// BoneMeal ...
+func (s Seagrass) BoneMeal(pos cube.Pos, w *world.World) bool {
+	if s.Type != DefaultSeagrass() {
+		return false
+	}
+
+	above := pos.Side(cube.FaceUp)
+	s2 := Seagrass{Type: TopSeagrass()}
+	if s2.canSurvive(above, w, true) {
+		s.Type = BottomSeagrass()
+		w.SetBlock(pos, s, nil)
+		w.SetBlock(above, s2, nil)
+		return true
+	}
+	return false
+}
+
+// UseOnBlock ...
+func (s Seagrass) UseOnBlock(pos cube.Pos, face cube.Face, _ mgl64.Vec3, w *world.World, user item.User, ctx *item.UseContext) bool {
+	pos, _, used := firstReplaceable(w, pos, face, s)
+	if !used {
+		return false
+	}
+	if !s.canSurvive(pos, w, true) {
+		return false
+	}
+
+	place(w, pos, s, user, ctx)
+	return placed(ctx)
+}
+
+// NeighbourUpdateTick ...
+func (s Seagrass) NeighbourUpdateTick(pos, _ cube.Pos, w *world.World) {
+	if !s.canSurvive(pos, w, false) {
+		w.SetBlock(pos, nil, nil)
+		if s.Type != DefaultSeagrass() {
+			var second cube.Pos
+			if s.Type == TopSeagrass() {
+				second = pos.Side(cube.FaceDown)
+			} else if s.Type == BottomSeagrass() {
+				second = pos.Side(cube.FaceUp)
+			}
+			if _, ok := w.Block(second).(Seagrass); ok {
+				w.SetBlock(second, nil, nil)
+			}
+		}
+	}
 }
 
 // HasLiquidDrops ...
@@ -28,17 +122,17 @@ func (Seagrass) SideClosed(cube.Pos, cube.Pos, *world.World) bool {
 
 // BreakInfo ...
 func (s Seagrass) BreakInfo() BreakInfo {
-	return newBreakInfo(0, alwaysHarvestable, nothingEffective, simpleDrops(item.NewStack(s, 1)))
+	return newBreakInfo(0, alwaysHarvestable, nothingEffective, func(t item.Tool, e []item.Enchantment) (stack []item.Stack) {
+		if t == nil || t.ToolType() == item.TypeShears {
+			stack = append(stack, item.NewStack(Seagrass{}, 1))
+		}
+		return stack
+	})
 }
 
 // FlammabilityInfo ...
 func (Seagrass) FlammabilityInfo() FlammabilityInfo {
 	return newFlammabilityInfo(0, 0, false)
-}
-
-// SmeltInfo ...
-func (Seagrass) SmeltInfo() item.SmeltInfo {
-	return newSmeltInfo(item.NewStack(item.Dye{Colour: item.ColourLime()}, 1), 0.1)
 }
 
 // EncodeItem ...
